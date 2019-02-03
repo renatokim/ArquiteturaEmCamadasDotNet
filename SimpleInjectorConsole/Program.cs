d8d6491748655e3ebb7c7b9cfe0a1ee54674bfd2
Renato Pereira <renato.kim@hotmail.com>
2019-02-03 18:22:40 -0200

    Instalação SimpleInjector 4.4.3

---
+++ SimpleInjectorConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using SimpleInjector;
+using Site.IRepositorio;
+using Site.IServico;
+using Site.Repositorio;
+using Site.Servico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInjectorConsole
+{
+    class Program
+    {
+        static Container container;
+
+        static void Main(string[] args)
+        {
+            Start();
+
+            //IFreteServico _freteServico = ServiceFactory.CreateInstance<IFreteServico>();
+            //var fretes = _freteServico.GetAll();
+
+            var handler = container.GetInstance<FreteServico>();
+            var fretesInj = handler.GetAll();
+
+        }
+
+        static void Start()
+        {
+            container = new Container();
+            container.Register<IFreteServico, FreteServico>(Lifestyle.Singleton);
+            container.Register<IFreteRepositorio, FreteRepositorio>(Lifestyle.Singleton);
+            
+            container.Verify();
+        }
+    }
+}
